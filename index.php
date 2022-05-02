fd3e8c80c3f5cf0c65f9c0d6e30cc5ecde87093d
Rohit Srivastava <70742576+Rohitsrivastva1@users.noreply.github.com>
2022-05-02 21:11:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="Student/style.css">
+
+    <title>Bu Online Voting System</title>
+  </head>
+  <body>
+
+<button href="login.php" target="_blank"><a href="Student/login.php" target="_blank">Students</a></button>
+
+<button href="admin/admin-login.php" target="_blank"><a href="admin/admin-login.php" target="_blank">Admin</a></button>
+
+  </body>
+</html>
